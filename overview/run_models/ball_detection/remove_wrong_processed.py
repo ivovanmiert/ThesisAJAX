14daeb96cc1c8a90d17a9a3710c2f4719f4d8bed
ivovanmiert <83641635+ivovanmiert@users.noreply.github.com>
2024-10-06 13:42:21 +0200

    Update remove_wrong_processed.py

--- overview/run_models/ball_detection/remove_wrong_processed.py
+++ overview/run_models/ball_detection/remove_wrong_processed.py
@@ -1,13 +1,10 @@
 import os
 import glob
 
-# Define the folder path
 folder_path = "/scratch-shared/ivanmiert/overview/ball_detection_processed"
 
-# Use glob to find all files ending with '_processed.csv'
 files_to_delete = glob.glob(os.path.join(folder_path, '*_processed.csv'))
 
-# Loop through and delete each file
 for file_path in files_to_delete:
     try:
         os.remove(file_path)
